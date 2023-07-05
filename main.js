0a407895a97b03da6d21b1c0e4a6192f04eca3a1
bokhodir <daikoziyev.baha@gmail.com>
2023-07-05 11:34:09 +0600

    Main branch ready to start

--- main.js
+++ main.js
@@ -1,13 +1,1 @@
-import './style.css'
-import { setupCounter } from './counter.js'
-
-document.querySelector('#app').innerHTML = `
-  <div>
-    <h1>Easy counter</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-  </div>
-`
-
-setupCounter(document.querySelector('#counter'))
+console.log("main js")

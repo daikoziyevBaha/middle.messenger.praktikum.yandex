e7444f0a6102976b5282a761624a63c36e1345d0
bokhodir <daikoziyev.baha@gmail.com>
2023-07-13 12:05:00 +0600

    input names added

--- src/components/Input/Input.tmpl.js
+++ src/components/Input/Input.tmpl.js
@@ -1,6 +1,6 @@
 export default `
     <label class="text-field">
-        <input class="text-field__input" type="text" value="{{value}}" placeholder=" "/>
+        <input name={{name}} class="text-field__input" type="text" value="{{value}}" placeholder=" "/>
         <span class="text-field__label">
             {{label}}
         </span>
